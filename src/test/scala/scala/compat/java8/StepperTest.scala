649dec44f2174348a7f12e14038cc6d0331a72ff
Rex Kerr <ichoran@gmail.com>
2015-09-07 23:42:46 -0700

    Moved classes outside of StepperTest to see if this helps exception.

--- src/test/scala/scala/compat/java8/StepperTest.scala
+++ src/test/scala/scala/compat/java8/StepperTest.scala
@@ -5,40 +5,40 @@ import org.junit.Assert._
 
 import collectionImpl._
 
+class IncStepperA(private val size0: Long) extends NextStepper[Int] {
+  if (size0 < 0) throw new IllegalArgumentException("Size must be >= 0L")
+  private var i = 0L
+  def characteristics = 0
+  def knownSize = math.max(0L, size0 - i)
+  def hasStep = i < knownSize
+  def nextStep() = { i += 1; (i - 1).toInt }
+  def substep() = if ((knownSize - i) <= 1) null else {
+    val sub = new IncStepperA(size0 - (size0 - i)/2)
+    sub.i = i
+    i = sub.size0
+    sub
+  }
+  def typedPrecisely = this
+  def spliterator = ???
+}
+
+class IncStepperB(private val size0: Long) extends TryStepper[Int] {
+  if (size0 < 0) throw new IllegalArgumentException("Size must be >= 0L")
+  private var i = 0L
+  def characteristics = 0
+  def knownSize = math.max(0L, size0 - i)
+  def tryStep(f: Int => Unit): Boolean = if (i >= size0) false else { f(i.toInt); i += 1; true }
+  def substep() = if ((knownSize - i) <= 1) null else {
+    val sub = new IncStepperB(size0 - (size0 - i)/2)
+    sub.i = i
+    i = sub.size0
+    sub
+  }
+  def typedPrecisely = this
+  def spliterator = ???
+}
+
 class StepperTest {
-  class IncStepperA(private val size0: Long) extends NextStepper[Int] with StepperLike[Int, IncStepperA] {
-    if (size0 < 0) throw new IllegalArgumentException("Size must be >= 0L")
-    private var i = 0L
-    def characteristics = 0
-    def knownSize = math.max(0L, size0 - i)
-    def hasStep = i < knownSize
-    def nextStep() = { i += 1; (i - 1).toInt }
-    def substep() = if ((knownSize - i) <= 1) null else {
-      val sub = new IncStepperA(size0 - (size0 - i)/2)
-      sub.i = i
-      i = sub.size0
-      sub
-    }
-    def typedPrecisely = this
-    def spliterator = ???
-  }
-
-  class IncStepperB(private val size0: Long) extends TryStepper[Int] with StepperLike[Int, IncStepperB] {
-    if (size0 < 0) throw new IllegalArgumentException("Size must be >= 0L")
-    private var i = 0L
-    def characteristics = 0
-    def knownSize = math.max(0L, size0 - i)
-    def tryStep(f: Int => Unit): Boolean = if (i >= size0) false else { f(i.toInt); i += 1; true }
-    def substep() = if ((knownSize - i) <= 1) null else {
-      val sub = new IncStepperB(size0 - (size0 - i)/2)
-      sub.i = i
-      i = sub.size0
-      sub
-    }
-    def typedPrecisely = this
-    def spliterator = ???
-  }
-
   def subs[Z, A, CC <: Stepper[A]](zero: Z)(s: Stepper[A])(f: Stepper[A] => Z, op: (Z, Z) => Z): Z = {
     val ss = s.substep()
     if (ss == null) op(zero, f(s))
