021490bce129cf5f521a6f2929b4607ec109e501
Rex Kerr <ichoran@gmail.com>
2015-12-21 20:38:25 -0800

    Got a little more of the code gen framework written.

--- benchmark/src/main/scala/bench/CodeGen.scala
+++ benchmark/src/main/scala/bench/CodeGen.scala
@@ -5,6 +5,7 @@ import scala.util.control.NonFatal
 
 object Generator {
   val names = "arr ish lst ils que stm trs vec arb ars ast mhs lhs prq muq wra jix jln".split(' ')
+
   def writeTo(f: java.io.File)(pr: (String => Unit) => Unit): Either[Throwable, Unit] = {
     try {
       val pw = new java.io.PrintWriter(f)
@@ -15,10 +16,21 @@ object Generator {
     }
     catch { case NonFatal(t) => Left(t) }
   }
+
+  def sayArrayI(oa: Option[Array[Int]]) = oa match { case Some(a) => a.mkString("Array(", ",", ")"); case _ => "" }
+
   def agreement(target: java.io.File, sizes: Option[Array[Int]] = None) {
     if (target.exists) throw new java.io.IOException("Generator will not write to existing file: " + target.getPath)
     writeTo(target){ pr =>
-      Seq("test").foreach(pr)
+      pr("""package bench.test""")
+      pr("""""")
+      pr("""object Agreement {""")
+      pr("""  def run() {""")
+      pr("""    val wrong = new collection.mutable.ArrayBuffer[String]""")
+      pr("""    def check[A](a1: A, a2: A, msg: String) = if (a1 != a2) wrong += msg""")
+      pr( s"    val x = new bench.generate.Things(${sayArrayI(sizes)})" )
+      pr("""  }""")
+      pr("""}""")
     } match {
       case Left(t) => println("Did not successfully write file: " + target.getPath); throw t
       case _ =>
