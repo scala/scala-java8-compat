36974288f63da33d401cbb853acb18f1ba8956c8
Seth Tisue <seth@tisue.net>
2018-05-03 11:26:34 +0200

    fix doc syntax

--- src/main/scala/scala/compat/java8/DurationConverters.scala
+++ src/main/scala/scala/compat/java8/DurationConverters.scala
@@ -64,14 +64,14 @@ object DurationConverters {
 
   implicit final class DurationOps(val duration: java.time.Duration) extends AnyVal {
     /**
-     * See [[DurationConverters.toScala]]
+     * See [[DurationConverters#toScala]]
      */
     def toScala: scala.concurrent.duration.FiniteDuration = DurationConverters.toScala(duration)
   }
 
   implicit final class FiniteDurationops(val duration: scala.concurrent.duration.FiniteDuration) extends AnyVal {
     /**
-     * See [[DurationConverters.toJava]]
+     * See [[DurationConverters#toJava]]
      */
     def toJava: java.time.Duration = DurationConverters.toJava(duration)
   }
